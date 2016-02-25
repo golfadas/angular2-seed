2c1ae95a353fb31d31b85667d012ea4eaff4bcdf
Bernardo Simoes <bersimoes@gmail.com>
2016-02-25 12:05:59 +0000

    Added simple component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,21 +1,10 @@
-import {Component, ViewEncapsulation} from 'angular2/core';
-import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
-import {NavbarComponent} from './navbar.component';
-import {ToolbarComponent} from './toolbar.component';
-import {HomeComponent} from '../../home/components/home.component';
-import {AboutComponent} from '../../about/components/about.component';
-import {NameListService} from '../../shared/services/name-list.service';
+import {Component} from 'angular2/core';
+import {TodoInput} from '../../todo/todo-input';
 
 @Component({
   selector: 'sd-app',
-  viewProviders: [NameListService],
-  moduleId: module.id,
-  templateUrl: './app.component.html',
-  encapsulation: ViewEncapsulation.None,
-  directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
+  directives: [TodoInput],
+  template: `<div><todo-input></todo-input></div>`
 })
-@RouteConfig([
-  { path: '/',      name: 'Home',  component: HomeComponent  },
-  { path: '/about', name: 'About', component: AboutComponent }
-])
+
 export class AppComponent {}
---
+++ src/todo/todo-input.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'todo-input',
+  template: `<div> Im a todo input</div>`
+})
+export class TodoInput {}
